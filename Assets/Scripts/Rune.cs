aa6f25e52a8a515d87baa56fcb01c9b83adcfb2a
Erica-99 <turbocake17@gmail.com>
2025-09-07 16:22:23 +1000

    Implemented basic rune class

--- Assets/Scripts/Rune.cs
+++ Assets/Scripts/Rune.cs
@@ -1,31 +1,62 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.VisualScripting;
 
-public class Rune : MonoBehaviour
+abstract public class Rune : MonoBehaviour
 {
 
-
     private List<GameObject> drawPoints;
-    private GameObject nextPoint;
+    private int nextPoint;
+
+    public bool completed;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        drawPoints = new List<GameObject>();
     }
 
     public void Initialize(List<Vector3> drawPointPositions, GameObject pointPrefab, bool debug=false)
     {
         foreach (Vector3 point in drawPointPositions)
         {
+            GameObject newSphere = Instantiate(pointPrefab, point, Quaternion.identity, transform);
+            if (debug)
+            {
+                newSphere.GetComponent<MeshRenderer>().enabled = true;
+            }
 
+            drawPoints.Add(newSphere);
         }
+
+        nextPoint = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    abstract public void DoSpell();
+
+    void ClearSpheres()
     {
-        
+        foreach (GameObject point in drawPoints)
+        {
+            Destroy(point);
+        }
+        drawPoints.Clear();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Collider sphereCollider = collision.GetContact(0).thisCollider;
+
+        if ( sphereCollider == drawPoints[nextPoint].GetComponent<Collider>())
+        {
+            drawPoints[nextPoint].GetComponent<Renderer>().material.color = new Color(0f, 1f, 0f, 0.75f);
+
+            nextPoint += 1;
+            if (nextPoint >= drawPoints.Count)
+            {
+                completed = true;
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Spells/TestSpell.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class TestSpell : Rune
+{
+    public override void DoSpell()
+    {
+        throw new System.NotImplementedException();
+    }
+}
